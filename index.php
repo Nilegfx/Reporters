5ee8b5e70378e669df9ecedf764190d6eb7c5dbb
Ahmed Ayoub <great.nile@gmail.com>
2014-03-02 19:33:47 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SD Reporters jQuery Plugin</title>
+	<script src="scripts/jquery-1.8.3.min.js"></script>
+	<script src="scripts/jquery.basic.plugin-boilerplate.js"></script>
+	<script src="scripts/scripts.js"></script>
+	<link rel="stylesheet" href="css/stylesheets/screen.css">
+</head>
+<body>
+	<div class="boxes-container">
+		<div class="box" data-id="1">
+			<div class="box-header">Header</div>
+			<!-- /.box-header -->
+			<div class="box-content">
+				Content
+				<a href="#" class="sd-getreporters">Click to get Reporters</a>
+			</div>
+			<!-- /.box-content -->
+			<div class="box-footer">Footer</div>
+			<!-- /.box-footer -->
+		</div>
+		<!-- /.box -->
+		<div class="box" data-id="2">
+			<div class="box-header">Header</div>
+			<!-- /.box-header -->
+			<div class="box-content">
+				Content
+				<a href="#" class="sd-getreporters">Click to get Reporters</a>
+			</div>
+			<!-- /.box-content -->
+			<div class="box-footer">Footer</div>
+			<!-- /.box-footer -->
+		</div>
+		<!-- /.box -->
+		<div class="box" data-id="3">
+			<div class="box-header">Header</div>
+			<!-- /.box-header -->
+			<div class="box-content">
+				Content
+				<a href="#" class="sd-getreporters">Click to get Reporters</a>
+			</div>
+			<!-- /.box-content -->
+			<div class="box-footer">Footer</div>
+			<!-- /.box-footer -->
+		</div>
+		<!-- /.box -->
+		<div class="box" data-id="4">
+			<div class="box-header">Header</div>
+			<!-- /.box-header -->
+			<div class="box-content">
+				Content
+				<a href="#" class="sd-getreporters">Click to get Reporters</a>
+			</div>
+			<!-- /.box-content -->
+			<div class="box-footer">Footer</div>
+			<!-- /.box-footer -->
+		</div>
+		<!-- /.box -->
+		<div class="box" data-id="5" data-nile="ahmed">
+			<div class="box-header">Header</div>
+			<!-- /.box-header -->
+			<div class="box-content">
+				Content
+				<a href="#" class="sd-getreporters">Click to get Reporters</a>
+			</div>
+			<!-- /.box-content -->
+			<div class="box-footer">Footer</div>
+			<!-- /.box-footer -->
+		</div>
+		<!-- /.box -->
+	</div>
+</body>
+</html>
